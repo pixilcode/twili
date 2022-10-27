3687d21af1d6557aa17017f9ffd8da72a12ff8c8
Brendon Bown <bhb49@byu.edu>
2022-10-27 16:08:07 -0600

    use list of tasks rather than hash tables

--- model.rkt
+++ model.rkt
@@ -1,50 +1,70 @@
 #lang racket/base
 (require racket/contract)
+(require gregor)
+(require "util.rkt")
 
 ; Structs
-(struct task (name due-date complete?) #:transparent)
+(struct task (name
+              group
+              unique-id
+              due-date
+              complete?)
+  #:transparent)
+
+(define (make-task name group day month year [complete? #f])
+  (define normalized-name (normalize name))
+  (define normalized-group (normalize group))
+  (define unique-id
+    (string-append normalized-name "-" normalized-group))
+  (define due-date (date year month day))
+  (task name group unique-id due-date complete?))
+  
 (provide (struct-out task))
-
-(struct group (list) #:transparent)
-(provide (struct-out group))
-
-; Getters
-(define (group-task-list group group-name)
-    (hash-ref (group-list group) group-name))
 
 ; Actions
 (module* actions #f
+  (module+ test
+    (require rackunit)
+    (define test-tasks (list (make-task
+                              "task a"
+                              "group-1"
+                              (->year (today))
+                              (->month (today))
+                              (->day (today))
+                              #f)
+                             (make-task
+                              "task b"
+                              "group-1"
+                              (->year (today))
+                              (->month (today))
+                              (->day (today))
+                              #f))))
+  
   ; add a task to the given group
-  (define (add-task target-group group-name task)
-    (define new-task-list
-      (append
-       (group-task-list target-group group-name)
-       (list task)))
-    (group (hash-set
-            (group-list target-group)
-            group-name
-            new-task-list)))
+  (define (add-task tasks new-task)
+    (append tasks (list new-task)))
 
   ; remove a task from a given group
-  (define (remove-task target-group group-name target-task-name)
-    (define new-task-list
-      (remove
-       target-task-name
-       (group-task-list target-group group-name)
-       (lambda (name task) (equal? name (task-name task)))))
-    (group (hash-set
-            (group-list target-group)
-            group-name
-            new-task-list)))
+  (define (remove-task tasks unique-id)
+    (remove
+     unique-id
+     tasks
+     (lambda (unique-id task) (equal?
+                               unique-id
+                               (task-unique-id task)))))
 
   ; list the tasks for a given group
-  (define list-group-tasks group-task-list)
+  (define (list-group-tasks tasks group)
+    (filter
+     (lambda (task) (equal? group (task-group task)))
+     tasks))
+
 
   ; list all tasks by group
-  (define list-all-tasks group-list)
+  (define (list-all-tasks tasks) tasks)
 
   (provide (contract-out
-            [add-task (-> group? string? task? group?)]
-            [list-group-tasks (-> group? string? (listof task?))]
-            [list-all-tasks (-> group? hash?)])))
-    
+            [add-task (-> (listof task?) task? (listof task?))]
+            [remove-task (-> (listof task?) string? (listof task?))]
+            [list-group-tasks (-> (listof task?) string? (listof task?))]
+            [list-all-tasks (-> (listof task?) (listof task?))])))
---
+++ util.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+(require racket/string)
+(require racket/contract)
+
+(module+ test
+  (require rackunit))
+
+;; convert a function to only contain alphanumerics and dashes
+(define (normalize string)
+  (string-trim
+   (string-replace
+    (string-downcase string) ; 1. convert the string to lower case
+    #rx"[^a-z0-9]+" ; 2. replace any non-alphanumeric numbers...
+    "-") ; ...with a dash
+   #rx"-+")) ; 3. remove any leading/trailing dashes
+(provide normalize)
+(module+ test
+  (check-equal? (normalize "abc") "abc"
+                "no special characters")
+  (check-equal? (normalize "Abc") "abc"
+                "to lower case")
+  (check-equal? (normalize "abc|d") "abc-d"
+                "remove special characters")
+  (check-equal? (normalize "abc def !") "abc-def-"
+                "remove trailing dashes"))
+
