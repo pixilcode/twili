4ce7c3241a80c58872415beee1c6b6bc2d3eeae9
Brendon Bown <brendon.bown@gmail.com>
2022-10-31 19:36:05 -0600

    Add due-date getters

--- model.rkt
+++ model.rkt
@@ -20,5 +20,18 @@
   (define due-date (date year month day))
   (task name group unique-id due-date complete?))
   
+; due date getters
+(define (task-due-date-day task)
+  (->day (task-due-date task)))
+
+(define (task-due-date-month task)
+  (->month (task-due-date task)))
+
+(define (task-due-date-year task)
+  (->year (task-due-date task)))
+
 (provide (struct-out task)
-         make-task)
+         make-task
+         task-due-date-day
+         task-due-date-month
+         task-due-date-year)
