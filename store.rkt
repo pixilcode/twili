bd1efc6821595be972bae3ce7eeb8163472814ec
Brendon Bown <brendon.bown@gmail.com>
2022-10-31 20:49:46 -0600

    Update store to work with task list

--- store.rkt
+++ store.rkt
@@ -1,36 +1,52 @@
 #lang racket/base
 (require yaml)
+(require racket/list)
 (require "model.rkt")
 
 (define default-yaml-file "test.yaml")
 
-(define (hash->task hash)
-  (define name (hash-ref hash "name"))
-  (define due-date (hash-ref hash "due-date"))
-  (define complete? (hash-ref hash "complete"))
-  (task name due-date complete?))
-
+; task/hash conversions
 (define (task->hash task)
+  (define due-date
+    (string-append (number->string (task-due-date-year task))
+                   "-"
+                   (number->string (task-due-date-month task))
+                   "-"
+                   (number->string (task-due-date-day task))))
   (make-hash
-   (list
-    (cons "name" (task-name task))
-    (cons "due-date" (task-due-date task))
-    (cons "complete" (task-complete? task)))))
+    (list
+      (cons "name" (task-name task))
+      (cons "due-date" due-date)
+      (cons "complete" (task-complete? task)))))
 
+(define (hash->task group-name)
+  (lambda (task-hash)
+    (define name (hash-ref task-hash "name"))
+    (define due-date (hash-ref task-hash "due-date"))
+    (define complete? (hash-ref task-hash "complete"))
+    (make-task name
+               group-name
+               (date-day due-date)
+               (date-month due-date)
+               (date-year due-date)
+               complete?)))
+
+; load/save yaml
 (define (load-from-yaml [file-loc default-yaml-file])
   (define file-input (file->yaml "test.yaml"))
-  (define groups
-    (hash-map/copy file-input
-                   (lambda (group-name task-list)
-                     (values group-name (map hash->task task-list)))))
-  (group groups))
+  (define tasks
+    (flatten (for/list ([(group-name task-list) (in-hash file-input)])
+               (map (hash->task group-name) task-list))))
+  tasks)
 
-(define (save-to-yaml groups [file-loc default-yaml-file])
-  (define groups-hash
-    (hash-map/copy (group-list groups)
-                   (lambda (group-name task-list)
-                     (values group-name (map task->hash task-list)))))
-  (write-yaml groups-hash
+(define (save-to-yaml task-list [file-loc default-yaml-file])
+  (define grouped-tasks (group-by task-group task-list))
+  (define grouped-tasks-hash (make-hash (map (lambda (tasks-group)
+                                               (displayln tasks-group)
+                                               (cons (task-group (first tasks-group))
+                                                     (map task->hash tasks-group)))
+                                             grouped-tasks)))
+  (write-yaml grouped-tasks-hash
               (open-output-file file-loc #:exists 'truncate/replace)
               #:style 'block))
 
